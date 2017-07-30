b1a24363aaa93c2e23715df99de0a9397919d047
gino0631 <gino0631@users.noreply.github.com>
2017-07-30 01:03:48 +0100

    Update MetadataControllerTest.php

--- tests/Controller/MetadataControllerTest.php
+++ tests/Controller/MetadataControllerTest.php
@@ -2,18 +2,26 @@
 namespace OCA\Metadata\Tests\Controller;
 
 use OCA\Metadata\Controller\MetadataController;
-use OCP\IRequest;
 use Test\TestCase;
 
 class MetadataControllerTest extends TestCase {
+    private $user;
     private $controller;
 
     public function setUp() {
         parent::setUp();
+        
+        $this->user = 'user_' . uniqid();
+        \OC_User::createUser($this->user, $this->user);
+
+        \OC\Files\Filesystem::tearDown();
+        \OC_User::setUserId($this->user);
+        \OC\Files\Filesystem::init($this->user, '/' . $this->user . '/files');
+        \OC\Files\Filesystem::clearMounts();
 
         $this->controller = new MetadataController(
             'metadata',
-            $this->createMock(IRequest::class)
+            $this->createMock(\OCP\IRequest::class)
         );
     }
 
