a368f6932ce25c10f7e32fef46530ed217ed8cf8
Tony Schmidt <tony@opensourceit.org>
2020-06-11 16:19:58 -0700

    created MetadataTagService

--- lib/Hooks/FileHooks.php
+++ lib/Hooks/FileHooks.php
@@ -2,13 +2,12 @@
 
 namespace OCA\Metadata\Hooks;
 
-use OCA\Metadata\Service\MetadataService;
+use OCA\Metadata\Service\MetadataTagService;
 
 class FileHooks {
 
 	public function onUpdatedFile(array $params) {
         /* TODO: (first iteration)
-         * - extract tag writing to "service"
          * - trigger queued job to write existing tags on update/install?
          * - create personal settings branch
          * - push WIP branch and start working on feature to share tagged photos
@@ -19,32 +18,12 @@ class FileHooks {
          * - trigger job when new metadata options selected
          */
 
-		$logger = \OC::$server->getLogger();
-        $logger->error("FUCKINNN FILE!!!!!!!!!!!");
-        $logger->error($params['path']);
-        $logger->error(json_encode($params));
-        $metadataService = new MetadataService();
-        $metadata = $metadataService->getMetadata($params['path'])->getArray();
-
-        $view = \OC\Files\Filesystem::getView();
-        $fileInfo = $view->getFileInfo($params['path']);
-        $logger->error($fileInfo->getType());
-        $fileId = $fileInfo->getId();
-        $objectType = $fileInfo->getType();
-
-        // convert IPTC keywords to tags
-        $tagIds = [];
-        foreach ($metadata['Keywords'] as $keyword) {
-            $tagManager = \OC::$server->getSystemTagManager();
-            $tag = $tagManager->getTag($keyword, True, True);
-            if (!$tag) {
-                $tag = $tagManager->createTag($keyword, True, True);
-            }
-            array_push($tagIds, $tag->getId());
+        $metadataTagService = new MetadataTagService();
+        $tagIds = $metadataTagService->getOrCreateTags($params['path']);
+        if ($tagIds) {
+            $metadataTagService->assignTags($tagIds);
         }
-        $tagObjectMapper = \OC::$server->getSystemTagObjectMapper();
-        // how are object types defined?
-        $tagObjectMapper->assignTags($fileId, 'files', $tagIds);
+
 	}
 
 }
