9383643263ed8234fe5e7ad14a2912863927948e
Tony Schmidt <tony@opensourceit.org>
2020-06-10 17:45:41 -0700

    WIP: fleshing out queued job for existing metadata->tags creation

--- lib/Cron/MetadataTagJob.php
+++ lib/Cron/MetadataTagJob.php
@@ -1,36 +1,38 @@
 <?php
 namespace OCA\Metadata\Cron;
 
-// use \OCA\MyApp\Service\SomeService;
 use \OCA\Metadata\Controller\MetadataController;
-use \OCP\BackgroundJob\TimedJob;
+use \OC\BackgroundJob\QueuedJob;
 use \OCP\IUserManager;
 use \OCP\ILogger;
 
-class MetadataTagJob extends TimedJob {
+class MetadataTagJob extends QueuedJob {
 
-    private $metadataController;
-
-	/** @var IUserManager */
-	protected $userManager;
-
-    public function __construct(ITimeFactory $time, MetadataController $metadataController, IUserManager $userManager, ILogger $logger) {
-        parent::__construct($time);
-        $this->metadataController = $metadataController;
-        $this->userManager = $userManager;
+    public function __construct() {
+		$logger = \OC::$server->getLogger();
         $this->logger = $logger;
-
-        // Run once every 5 minutes
-        //parent::setInterval(300);
-        parent::setInterval(5);
+		$this->userManager = \OC::$server->getUserManager();
+        $this->filesystem = new \OC\Files\View('/' . 'tony');
+        $metadataService = new MetadataService();
     }
 
-    public static function run($arguments) {
-        // $this->metadataController->doCron($arguments['uid']);
-        $user = $this->userManager->get($arguments['uid']);
-        throw new \Exception("user is $user");
+    protected function run($arguments) {
         print("WTF!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-        $this->logger->error("WTF DUDE!!!!");
+        /*
+        $this->logger->error("ARGUMENTS: $arguments");
+        $this->logger->error(json_encode(get_class_methods($this->userManager)));
+        $users = $this->userManager->search('');
+        $user = $users['tony'];
+        $this->logger->error("UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUSER:");
+        $this->logger->error(json_encode($users));
+        $this->logger->error(json_encode(get_class_methods($user)));
+         */
+        $this->logger->error("FIIIIIIIIIIIIIIIIIILES:");
+        $files = $this->filesystem->getDirectoryContent('/files');
+        $this->logger->error(json_encode(get_class_methods($files[0])));
+        $this->logger->error(json_encode($files[0]->getPath()));
+        // TODO: extract metadata tag creation file hook into service and
+        // recurse users, files and directories from here
     }
 
 }
